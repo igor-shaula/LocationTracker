7113d5316d4ef2ff6c80197980b3504bce2a86d3
igor-shaula <igor-shaula@gmail.com>
2016-05-03 23:32:00 +0300

    build & SDK version lowered to 22 - to avoid problems with camera on Android 6. Now all works fine :)

--- app/src/main/java/com/solveast/gps_tracker/MyLog.java
+++ app/src/main/java/com/solveast/gps_tracker/MyLog.java
@@ -6,23 +6,28 @@ public class MyLog {
 
     private static final String TAG = "LOG";
 
+    @SuppressWarnings("unused")
     public static void v(String message) {
         Log.v(TAG, message);
     }
 
+    @SuppressWarnings("unused")
     public static void d(String message) {
         Log.d(TAG, message);
     }
 
+    @SuppressWarnings("unused")
     public static void i(String message) {
-//        Log.i(TAG, message);
+        Log.i(TAG, message);
     }
 
+    @SuppressWarnings("unused")
     public static void w(String message) {
-//        Log.w(TAG, message);
+        Log.w(TAG, message);
     }
 
+    @SuppressWarnings("unused")
     public static void e(String message) {
-//        Log.e(TAG, message);
+        Log.e(TAG, message);
     }
 }
